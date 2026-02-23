e5ad1763b05ca9b09dd63ab29c156f9a81ed7b3a
ABC-NAT <ippromex12@gmail.com>
2026-02-23 20:37:40 +0700

    Initail commit

---
+++ src/main/java/com/example/studentregistraction/StudentregistractionApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentregistraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentregistractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentregistractionApplication.class, args);
+	}
+
+}
